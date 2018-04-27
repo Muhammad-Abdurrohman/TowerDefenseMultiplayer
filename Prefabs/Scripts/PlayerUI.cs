127551dbbd99d0030c9da33c1bd2b5972e5dcd0b
Muhammad-Abdurrohman <Muhammad-Abdurrohman@users.noreply.github.com>
2018-04-27 17:07:33 +0700

    Add files via upload

---
+++ Prefabs/Scripts/PlayerUI.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerUI : MonoBehaviour {
+
+    [SerializeField]
+    GameObject pauseMenu;
+
+	// Use this for initialization
+	void Start () {
+        pauseMenu.SetActive(false);
+        PauseMenu.IsOn = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.Backspace))
+        {
+            TogglePauseMenu();
+        }
+	}
+
+    void TogglePauseMenu()
+    {
+        pauseMenu.SetActive(!pauseMenu.activeSelf);
+        PauseMenu.IsOn = pauseMenu.activeSelf;
+    }
+}
